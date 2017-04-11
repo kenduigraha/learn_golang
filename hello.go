8a9f5a47c0e0f4582020648bb749199b61b99ade
kenduigraha <ken.duigraha@kudo.co.id>
2017-04-11 15:26:29 +0700

    checkpoint

--- hello.go
+++ hello.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "html/template"
+        "net/http"
+        "time"
+        "hash/crc32"
+)
+
+type World struct {}
+
+func (w * World) String() string {
+  return "World 2"
+}
+
+// type Office int
+
+// const (
+//   Boston Office = iota
+//   NewYork
+// )
+
+// func (o Office) String() string {
+//   return "Google, " + officePlace[o]
+// }
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf("Hello, %s\n", "World 1")
+
+    fmt.Printf("Hello, %s\n", new(World))
+
+    // fmt.Printf("Hello, %s\n", Boston)
+
+    day := time.Now().Weekday()
+    fmt.Printf("Hello, %s (%d)\n", day, day)
+
+    start := time.Now()
+
+    http.Get("http://www.google.com/")
+
+    fmt.Println(time.Since(start))
+
+    h := crc32.NewIEEE()
+
+    fmt.Fprintf(h, "Hello Wordld\n")
+    fmt.Println("hash=%#x\n", h.Sum32())
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        var data = map[string]string{
+            "Name":    "john wick",
+            "Message": "have a nice day",
+        }
+
+        var t, err = template.ParseFiles("template.html")
+        if err != nil {
+            fmt.Println(err.Error())
+        }
+
+        t.Execute(w, data)
+    })
+
+    fmt.Println("starting web server at http://localhost:8080/")
+    http.ListenAndServe(":8080", nil)
 }
